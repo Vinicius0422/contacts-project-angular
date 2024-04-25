8c490459350222fcaedc9bfe5e3e57f0c60dcb7b
Vinicius0422 <guedescosta123@hotmail.com>
2024-04-25 20:28:05 -0300

    refactor: change contact routing

--- src/app/components/contacts/contacts-routing.module.ts
+++ src/app/components/contacts/contacts-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { NewComponent } from './new/new.component';
-import { UpdateComponent } from './update/update.component';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   { path: '', component: ContactListComponent },
-  { path: 'new', component: NewComponent },
-  { path: 'update', component: UpdateComponent },
+  { path: 'new', component: FormComponent },
+  { path: 'update', component: FormComponent },
   { path: 'details/:id', component: DetailsComponent },
 ];
 
