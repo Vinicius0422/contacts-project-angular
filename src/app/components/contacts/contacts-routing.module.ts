d0f716df5275b4da8b4d64915045540dbf82c6d8
Vinicius0422 <guedescosta123@hotmail.com>
2024-04-30 12:43:10 -0300

    refactor: fix path to update

--- src/app/components/contacts/contacts-routing.module.ts
+++ src/app/components/contacts/contacts-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { DetailsComponent } from './details/details.component';
 import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', component: ContactListComponent },
   { path: 'new', component: FormComponent },
-  { path: 'update', component: FormComponent },
+  { path: 'update/:id', component: FormComponent },
   { path: 'details/:id', component: DetailsComponent },
 ];
 
