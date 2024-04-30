b8c5acc1aaec421c72c7258165a7dd10c5bfa317
Vinicius0422 <guedescosta123@hotmail.com>
2024-04-30 12:36:28 -0300

    feat: add not-found to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
 
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'contacts', loadChildren: () => import('./components/contacts/contacts.module').then(m => m.ContactsModule)},
-    {path: 'profile', component: ProfileComponent}
+    {path: 'profile', component: ProfileComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
