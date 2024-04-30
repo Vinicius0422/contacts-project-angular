9d3d7f0ab14a864da3be5c1712878e17277a5093
Vinicius0422 <guedescosta123@hotmail.com>
2024-04-30 12:41:05 -0300

    feat: implement details component logic

--- src/app/components/contacts/details/details.component.ts
+++ src/app/components/contacts/details/details.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts.service';
+import { Observable, switchMap } from 'rxjs';
+import { Contact } from 'src/app/models/contact.interface';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  contact$!: Observable<Contact>
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: ContactsService
+  ) {}
 
   ngOnInit(): void {
+    this.contact$ = this.route.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        return this.service.getContactById(id)
+      })
+    );
   }
-
 }
